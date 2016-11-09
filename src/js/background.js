3aa6b73e89c40a336bbbfb995a5153a3aeb91283
Coop <coop.coding@gmail.com>
2016-11-09 11:15:49 +1100

    Changed the main event listeners and how respond to their events.

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,6 @@
 require('file?name=manifest.[ext]!../manifest.json')
 
-import got from 'got'
+// import got from 'got'
 
 const extensionOptionsDefaultValues = {
   integrateWithBaiduSearch: true,
@@ -11,7 +11,7 @@ const extensionOptionsDefaultValues = {
 }
 let marksearchServerAddress = null
 let marksearchApiToken = null
-let urlThatWasLastChecked = null
+// let urlThatWasLastChecked = null
 
 function assignServerAddressAndToken(extensionTokenString){
   if(typeof extensionTokenString === 'string' && extensionTokenString.indexOf(',') > 1){
@@ -30,80 +30,101 @@ function assignServerAddressAndToken(extensionTokenString){
 */
 chrome.storage.local.get(null, ({extensionToken}) => assignServerAddressAndToken) // eslint-disable-line no-unused-vars
 
+// function updateIcon(){
+//
+// }
+
 /*****
 * Check if the web page is saved in MarkSearch
 */
-function checkIfPageIsSaved(){
+function checkIfPageIsSaved(tabId){
+  console.log('***********************===============================********************')
   console.log('checkIfPageIsSaved 1')
 
   if(!marksearchServerAddress || !marksearchApiToken){
     return
   }
-  console.log('checkIfPageIsSaved 2')
-  console.log(marksearchApiToken)
-  console.log(marksearchServerAddress)
-  chrome.tabs.query(
-    {
-      active: true,
-      currentWindow: true,
-      windowType: 'normal'
-    },
-    tabs => {
-      if(!tabs[0] || !tabs[0].url){
-        return
-      }
-      const currentTab = tabs[0]
-      const currentUrl = currentTab.url
-      console.log('urlThatWasLastChecked', urlThatWasLastChecked)
-      if(currentUrl === urlThatWasLastChecked || !/^(http|https):\/\//.test(currentUrl)){
-        return
-      }
-      console.log('chrome.tabs.query callback')
-      /* eslint-disable */
-      // got
-      //   .post(
-      //     marksearchServerAddress + '/api/get/' + encodeURIComponent(currentUrl),
-      //     {
-      //       headers: {
-      //         Authorization: marksearchApiToken
-      //       }
-      //     }
-      //   )
-      //   .then(response => {
-      //     console.log('got.post success')
-      //     urlThatWasLastChecked = currentUrl
-      //     console.log(response.body)
-      //   })
-      //   .catch(error => {
-      //     console.log('got.post error')
-      //     console.error(error)
-      //   })
-        // got('http://127.0.0.1', {port: 8080})
-        // // got('http://www.bom.gov.au/products/IDR023.loop.shtml#skip')
-        //   .then(response => {
-        //     console.log('got.post success')
-        //     urlThatWasLastChecked = currentUrl
-        //     console.log(response.body)
-        //   })
-        //   .catch(error => {
-        //     console.log('got.post error')
-        //     console.error(error)
-        //   })
-        fetch('http://127.0.0.1:8080', {
-          method: 'get'
-        }).then(function(response) {
-            console.log('fetch success')
-            console.log(response)
-        }).catch(function(err) {
-          console.log('fetch success')
-          console.error(err)
-        })
-        /* eslint-enable */
+  // console.log('checkIfPageIsSaved 2')
+  // console.log(marksearchApiToken)
+  // console.log(marksearchServerAddress)
+  chrome.tabs.get(tabId, tab => {
+    if(!/^(http|https):\/\//.test(tab.url)){
+      return
     }
-  )
+    console.log('tab.url', tab.url)
+    fetch('http://127.0.0.1:8080', {
+      method: 'get'
+    }).then(function(response) {
+      console.log('fetch success')
+      console.log(response)
+    }).catch(function(err) {
+      console.log('fetch success')
+      console.error(err)
+    })
+  })
+  // chrome.tabs.query(
+  //   {
+  //     active: true,
+  //     currentWindow: true,
+  //     windowType: 'normal'
+  //   },
+  //   tabs => {
+  //     console.log('tabs', tabs)
+  //     if(!tabs[0] || !tabs[0].url){
+  //       return
+  //     }
+  //     const currentTab = tabs[0]
+  //     const currentUrl = currentTab.url
+  //     console.log('currentTab', currentTab)
+  //     console.log('currentUrl', currentUrl)
+  //
+  //     if(!/^(http|https):\/\//.test(currentUrl)){
+  //       return
+  //     }
+  //     console.log('chrome.tabs.query callback')
+  //     /* eslint-disable */
+  //     got
+  //       .post(
+  //         marksearchServerAddress + '/api/get/' + encodeURIComponent(currentUrl),
+  //         {
+  //           headers: {
+  //             Authorization: marksearchApiToken
+  //           }
+  //         }
+  //       )
+  //       .then(response => {
+  //         console.log('got.post success')
+  //         urlThatWasLastChecked = currentUrl
+  //         console.log(response.body)
+  //       })
+  //       .catch(error => {
+  //         console.log('got.post error')
+  //         console.error(error)
+  //       })
+  //       got('http://127.0.0.1', {port: 8080})
+  //       // got('http://www.bom.gov.au/products/IDR023.loop.shtml#skip')
+  //         .then(response => {
+  //           console.log('got.post success')
+  //           urlThatWasLastChecked = currentUrl
+  //           console.log(response.body)
+  //         })
+  //         .catch(error => {
+  //           console.log('got.post error')
+  //           console.error(error)
+  //         })
+  //       fetch('http://127.0.0.1:8080', {
+  //         method: 'get'
+  //       }).then(function(response) {
+  //           console.log('fetch success')
+  //           console.log(response)
+  //       }).catch(function(err) {
+  //         console.log('fetch success')
+  //         console.error(err)
+  //       })
+  //       /* eslint-enable */
+  //   }
+  // )
 }
-
-checkIfPageIsSaved()
 
 /*****
 * Event listeners
@@ -127,19 +148,41 @@ chrome.runtime.onInstalled.addListener(({reason}) => {
   }
 })
 
-chrome.tabs.onActivated.addListener(checkIfPageIsSaved)
+// chrome.tabs.onActivated.addListener(() => checkIfPageIsSaved)
+// chrome.tabs.onUpdated.addListener((tabId, {status}) => {
+//   /*****
+//   * Start it early so by the time its loaded we have (hopefully) received a response from
+//   * the MarkSearch server and the icon has been changed to reflect its status as saved/not saved.
+//   */
+//   if(status === 'loading'){
+//     checkIfPageIsSaved()
+//   }
+// })
+// chrome.windows.onFocusChanged.addListener(() => checkIfPageIsSaved)
 
-chrome.tabs.onUpdated.addListener((tabId, {status}) => {
-  /*****
-  * Start it early so by the time its loaded we have (hopefully) received a response from
-  * the MarkSearch server and the icon has been changed to reflect its status as saved/not saved.
-  */
-  if(status === 'loading'){
-    checkIfPageIsSaved()
+chrome.tabs.onActivated.addListener(({tabId}) => checkIfPageIsSaved) // eslint-disable-line no-unused-vars
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if(tab.highlighted) {
+    checkIfPageIsSaved(tabId)
   }
 })
 
-chrome.windows.onFocusChanged.addListener(checkIfPageIsSaved)
+chrome.windows.onFocusChanged.addListener(() => {
+  chrome.windows.getCurrent({
+    populate: true
+  },
+  window => {
+    if(window && Array.isArray(window.tabs)) {
+      for(const tab of window.tabs) {
+        if(tab.highlighted) {
+          console.log('onFocusChanged highlighted')
+          checkIfPageIsSaved(tab.id)
+        }
+      }
+    }
+  })
+})
 
 /*****
 * If user changes the token, update the reference for server address and server api token
