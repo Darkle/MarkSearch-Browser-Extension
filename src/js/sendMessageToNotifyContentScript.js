b7f9ab014e046f1ce9f9081be735b302ac52e28d
Coop <coop.coding@gmail.com>
2016-11-12 21:16:43 +1100

    Switchted to using getCurrentTabId in sendMessageToNotifyContentScript

--- src/js/sendMessageToNotifyContentScript.js
+++ src/js/sendMessageToNotifyContentScript.js
@@ -1,18 +1,16 @@
 
+import { getCurrentTabId } from './utils'
+
 /*****
 * sendMessageToNotifyContentScript sends a message to savePageAndNotify_ContentScript so it can inform the user
 * as to the success/failure of saving the page to MarkSearch.
 */
 function sendMessageToNotifyContentScript({action, actionSucceeded, errorMessage, noToken, notifyScriptRunningCheck}){
   return new Promise( resolve => {
-    chrome.tabs.query(
-      {
-        active: true,
-        currentWindow: true
-      },
-      tabs => {
+    getCurrentTabId()
+      .then(tabId => {
         chrome.tabs.sendMessage(
-          tabs[0].id,
+          tabId,
           {
             action,
             actionSucceeded,
@@ -26,8 +24,7 @@ function sendMessageToNotifyContentScript({action, actionSucceeded, errorMessage
           */
           resolve
         )
-      }
-    )
+      })
   })
 }
 
