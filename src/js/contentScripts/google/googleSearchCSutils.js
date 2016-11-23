842943c334ed3070b55c4e21cf98f493196e7ee0
Coop <coop.coding@gmail.com>
2016-11-23 17:10:32 +1100

    Comment

--- src/js/contentScripts/google/googleSearchCSutils.js
+++ src/js/contentScripts/google/googleSearchCSutils.js
@@ -8,7 +8,7 @@ const pageQueryParams = new URLSearchParams(window.location.search.slice(1))
 * If there is a query string (?), then it's not instant search. Can't do it the
 * other way, cause it's possible to have a hash on the end of a query string, but not vice versa (AFAIK)
 */
-const isInstantSearch = !pageQueryParams.has('q')    //if there is a query string (?), then it's not instant search
+const isInstantSearch = !pageQueryParams.has('q')
 
 function getPageHash(){
   /*****
